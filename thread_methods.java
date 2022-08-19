7470e092e3244ba1f80911d0d9136a587916ff76
Harshil Savaj <savajharshil99@gmail.com>
2022-08-19 21:08:23 +0530

    Advanced

---
+++ thread_methods.java
@@ -0,0 +1,10 @@
+public class thread_methods {
+   public void MyNewThr1(){
+
+   }
+    public static void main(String[] args) {
+        System.out.println("Java threads"); 
+    }
+}
+
+
---
+++ trycatch.java
@@ -0,0 +1,13 @@
+public class trycatch {
+    public static void main(String[] args) {
+        int a =6000;
+        int b = 0;
+        try{
+            int c=a/b;
+            System.out.println("The Result is "+c);
+        }
+        catch(Exception e){
+            System.out.println("We fetch the error as :"+e);
+        }      
+    }
+}
