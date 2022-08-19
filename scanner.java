0f619bef94d38f60ff6f60401aa7beccdd18b318
Harshil Savaj <savajharshil99@gmail.com>
2022-08-19 21:59:44 +0530

    advanced

---
+++ scanner.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class scanner {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        
+        System.out.println("Enter the value of a,b,c");
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+
+
+        System.out.println("The Scanned number is  :");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    
+    }
+}
--- stack.java
+++ stack.java
@@ -1,16 +1,29 @@
-package p1;
-
+import java.util.*;
+import java.io.*;
 public class stack {
+    
+    static void s_push(Stack <Integer> stack){
+        System.out.println("Current Elements inside stack ");
+            for(int i=0;i<5;i++){
+                stack.push(i);
+                System.out.println(i);
+            }
+    }
+
+    static void s_pop(Stack<Integer> stack){
+        System.out.println("Poping each elemnt in LIFO manner :");
+
+        for(int i=0;i<5;i++){
+            Integer y=(Integer) stack.pop();
+            System.out.println(y);
+        }
+    }
 
     public static void main(String []args){
-        
-        Stack nums=new Stack();
-        nums.push(15);
-        nums.push(12);
-        nums.push(8);
+        Stack<Integer> stack=new Stack<Integer>();
 
-        nums.show();
-
+        s_push(stack);
+        s_pop(stack);
 
 
     }
