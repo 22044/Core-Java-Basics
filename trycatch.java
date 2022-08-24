d21e9081fcd9a212780d555908da381ffe82f592
Harshil Savaj <savajharshil99@gmail.com>
2022-08-24 19:20:52 +0530

    trycatch

--- trycatch.java
+++ trycatch.java
@@ -1,7 +1,7 @@
 public class trycatch {
     public static void main(String[] args) {
         int a =6000;
-        int b = 0;
+        int b = 98;
         try{
             int c=a/b;
             System.out.println("The Result is "+c);
